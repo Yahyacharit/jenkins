ce2437fc3d9e410e4143d20c281dd1a7ad15c872
YahyaCHARIT <yahya.charit2018@gmail.com>
2024-12-17 23:58:13 +0100

    Initial commit

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, CI/CD Pipeline!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
